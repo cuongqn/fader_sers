bd4159e7a130cdaac6db3bf3fcd8c232d3954cd2
Cuong Nguyen <cuongnguyen@Cuongs-MacBook-Pro.local>
2019-05-28 15:05:38 -0700

    add data preprocessing

---
+++ fader_networks/models/download.sh
@@ -0,0 +1,7 @@
+curl -Lo classifier128.pth https://dl.fbaipublicfiles.com/FaderNetworks/classifier128.pth
+curl -Lo classifier256.pth https://dl.fbaipublicfiles.com/FaderNetworks/classifier256.pth
+curl -Lo eyeglasses.pth https://dl.fbaipublicfiles.com/FaderNetworks/eyeglasses.pth
+curl -Lo male.pth https://dl.fbaipublicfiles.com/FaderNetworks/male.pth
+curl -Lo narrow_eyes.pth https://dl.fbaipublicfiles.com/FaderNetworks/narrow_eyes.pth
+curl -Lo young.pth https://dl.fbaipublicfiles.com/FaderNetworks/young.pth
+curl -Lo pointy_nose.pth https://dl.fbaipublicfiles.com/FaderNetworks/pointy_nose.pth
---
+++ src/preprocess.py
@@ -0,0 +1,56 @@
+# %%
+import pandas as pd
+import numpy as np
+import os
+import functools
+from scipy.signal import butter, lfilter, filtfilt
+from scipy.stats import zscore
+
+
+data_dir = "data/raw/"
+concentrations = os.listdir(data_dir)
+
+
+class SERSPreprocessor(object):
+    def get_spectra_numpy(
+        self,
+        file_dir_list,
+        sep="\t",
+        header=None,
+        names=["x", "y", "wavenumber", "intensity"],
+    ):
+        load_func = functools.partial(
+            self._preprocess_single_file, sep=sep, header=header, names=names
+        )
+        spectra_arr = list(map(load_func, file_dir_list))
+        spectra_arr = np.concatenate(spectra_arr)
+
+        highpass_func = functools.partial(
+            self.butter_bandpass_filter, lowcut=30, highcut=5000, fs=20001, order=1
+        )
+        spectra_arr = np.array(list(map(highpass_func, spectra_arr)))
+
+        spectra_arr = zscore(spectra_arr, axis=1)
+        return spectra_arr
+
+    @staticmethod
+    def butter_bandpass_filter(data, highcut, lowcut, fs, order=5):
+        nyq = 0.5 * fs
+        low = lowcut / nyq
+        high = highcut / nyq
+        b, a = butter(order, low, btype="highpass")
+        data = filtfilt(b, a, data)
+        b, a = butter(order, high, btype="lowpass")
+        data = filtfilt(b, a, data)
+        return data
+
+    @staticmethod
+    def _preprocess_single_file(file_dir, sep, header, names):
+        data = pd.read_csv(file_dir, sep=sep, header=header, names=names)
+        spectra_length = int(len(data) / data[names[-2]].value_counts().iloc[0])
+        spectra_arr = data[names[-1]].values.reshape(-1, spectra_length)
+        return spectra_arr
+
+if __name__ == "__main__":
+    file_dir_list = [data_dir + conc for conc in ["0.txt", "100000.txt"]]
+    spectra_arr = SERSPreprocessor().get_spectra_numpy(file_dir_list)
